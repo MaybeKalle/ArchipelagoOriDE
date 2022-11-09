d00278e5ae3403d158dc90c56cd192fcd2bf1f4c
Frederik <15572935+MaybeKalle@users.noreply.github.com>
2022-11-09 23:51:09 +0100

    Added Messages For every Pickup event added a message that is displayed in game. For the experience also check if the pickup is a orb placed in the world.

--- ArchipelagoOriDE/Patches/PickupPatcher.cs
+++ ArchipelagoOriDE/Patches/PickupPatcher.cs
@@ -11,11 +11,6 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectKeystonePickupPatch(KeystonePickup keystonePickup)
         {
             if (!State.ModActive) return true;
-            
-            ItemMessageProvider provider = new ItemMessageProvider();
-
-            string keystonePosition = "[" +  keystonePickup.Bounds.center.x + " | " +  keystonePickup.Bounds.center.y + " | " +  keystonePickup.Bounds.center.z + "]";
-            
             Main.MessageQueue.AddMessage("You $collected$ a #Keystone#");
             return true;
         }
@@ -23,49 +18,51 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectSkillPointPickupPatch(SkillPointPickup skillPointPickup)
         {
             if (!State.ModActive) return true;
-            
+            Main.MessageQueue.AddMessage("You $collected$ a #Skill Point#");
             return true;
         }
 
         public static bool OnCollectEnergyOrbPickupPatch(EnergyOrbPickup energyOrbPickup)
         {
             if (!State.ModActive) return true;
-            
+            Main.MessageQueue.AddMessage("You $collected$ an #Energy Orb#");
             return true;
         }
 
         public static bool OnCollectMaxEnergyContainerPickupPatch(MaxEnergyContainerPickup energyContainerPickup)
         {
             if (!State.ModActive) return true;
-            
+            Main.MessageQueue.AddMessage("You $collected$ an #Energy Cell#");
             return true;
         }
 
         public static bool OnCollectExpOrbPickupPatch(ExpOrbPickup expOrbPickup)
         {
             if (!State.ModActive) return true;
-            
+            if (expOrbPickup.MessageType == ExpOrbPickup.ExpOrbMessageType.None) return true;
+            Main.Logger.Log("Collected Experience Orb: " + expOrbPickup.Amount + " - Type: " + expOrbPickup.MessageType);
+            Main.MessageQueue.AddMessage("You $collected$ an #Experience Orb#");
             return true;
         }
 
         public static bool OnCollectMaxHealthContainerPickupPatch(MaxHealthContainerPickup maxHealthContainerPickup)
         {
             if (!State.ModActive) return true;
-            
+            Main.MessageQueue.AddMessage("You $collected$ a #Health Cell#");
             return true;
         }
 
         public static bool OnCollectRestoreHealthPickupPatch(RestoreHealthPickup restoreHealthPickup)
         {
             if (!State.ModActive) return true;
-            
+            Main.MessageQueue.AddMessage("You $collected$ a #Health Restore#");
             return true;
         }
 
         public static bool OnCollectMapStonePickupPatch(MapStonePickup mapStonePickup)
         {
             if (!State.ModActive) return true;
-            
+            Main.MessageQueue.AddMessage("You $collected$ a #Map Stone#");
             return true;
         }
     }
