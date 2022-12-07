7aff3f11bf855f62a13713dda9d5ccfbbba2247e
Frederik <15572935+MaybeKalle@users.noreply.github.com>
2022-12-07 23:34:22 +0100

    Removed debug messages

--- ArchipelagoOriDE/Patches/PickupPatcher.cs
+++ ArchipelagoOriDE/Patches/PickupPatcher.cs
@@ -10,7 +10,6 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectKeystonePickupPatch(KeystonePickup keystonePickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ a #Keystone#");
             Main.Logger.Log("[Collect] " + RandomizerUtility.GenerateObjectJson(keystonePickup).ToString(Formatting.None));
             return true;
         }
@@ -18,7 +17,6 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectSkillPointPickupPatch(SkillPointPickup skillPointPickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ a #Skill Point#");
             Main.Logger.Log("[Collect] " + RandomizerUtility.GenerateObjectJson(skillPointPickup).ToString(Formatting.None));
             return true;
         }
@@ -26,14 +24,12 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectEnergyOrbPickupPatch(EnergyOrbPickup energyOrbPickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ an #Energy Orb#");
             return true;
         }
 
         public static bool OnCollectMaxEnergyContainerPickupPatch(MaxEnergyContainerPickup energyContainerPickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ an #Energy Cell#");
             Main.Logger.Log("[Collect] " + RandomizerUtility.GenerateObjectJson(energyContainerPickup).ToString(Formatting.None));
             return true;
         }
@@ -42,7 +38,6 @@ namespace OriForestArchipelago.Patches
         {
             if (!State.ModShouldInteract()) return true;
             if (expOrbPickup.MessageType == ExpOrbPickup.ExpOrbMessageType.None) return true;
-            Main.MessageQueue.AddMessage("You $collected$ an #Experience Orb#");
             Main.Logger.Log("[Collect] " + RandomizerUtility.GenerateObjectJson(expOrbPickup).ToString(Formatting.None));
             return true;
         }
@@ -50,7 +45,6 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectMaxHealthContainerPickupPatch(MaxHealthContainerPickup maxHealthContainerPickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ a #Health Cell#");
             Main.Logger.Log("[Collect] " + RandomizerUtility.GenerateObjectJson(maxHealthContainerPickup).ToString(Formatting.None));
             return true;
         }
@@ -58,14 +52,12 @@ namespace OriForestArchipelago.Patches
         public static bool OnCollectRestoreHealthPickupPatch(RestoreHealthPickup restoreHealthPickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ a #Health Restore#");
             return true;
         }
 
         public static bool OnCollectMapStonePickupPatch(MapStonePickup mapStonePickup)
         {
             if (!State.ModShouldInteract()) return true;
-            Main.MessageQueue.AddMessage("You $collected$ a #Map Stone#");
             Main.Logger.Log("[Collect] " + RandomizerUtility.GenerateObjectJson(mapStonePickup).ToString(Formatting.None));
             return true;
         }
