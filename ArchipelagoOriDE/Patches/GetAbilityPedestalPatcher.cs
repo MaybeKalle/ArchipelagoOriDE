0f4f35aba9aeccc29ee9f297f314da9af605f7c0
Frederik <15572935+MaybeKalle@users.noreply.github.com>
2022-12-22 21:01:13 +0100

    Fixed bug The activation of the Spirit Tree causes problems with the camera movement and the interaction in general

--- ArchipelagoOriDE/Patches/GetAbilityPedestalPatcher.cs
+++ ArchipelagoOriDE/Patches/GetAbilityPedestalPatcher.cs
@@ -7,7 +7,6 @@ namespace OriForestArchipelago.Patches
         public static bool ActivatePedestalPatch(ref GetAbilityPedestal __instance)
         {
             __instance.CurrentState = GetAbilityPedestal.States.Completed;
-            __instance.ActivatePedestalSequence.Perform((IContext) null);
             Main.RandomizerSession.CollectedCheck(__instance.Ability);
             return false;
         }
