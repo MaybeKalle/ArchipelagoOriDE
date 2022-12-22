5f4fd0573fbb67021542a3fb1bfc6c179c593bce
Frederik <15572935+MaybeKalle@users.noreply.github.com>
2022-12-22 21:24:41 +0100

    Ignore Spirit Trees if inactive

--- ArchipelagoOriDE/Patches/GetAbilityPedestalPatcher.cs
+++ ArchipelagoOriDE/Patches/GetAbilityPedestalPatcher.cs
@@ -6,6 +6,7 @@ namespace OriForestArchipelago.Patches
     {
         public static bool ActivatePedestalPatch(ref GetAbilityPedestal __instance)
         {
+            if (!State.ModShouldInteract()) return true;
             __instance.CurrentState = GetAbilityPedestal.States.Completed;
             Main.RandomizerSession.CollectedCheck(__instance.Ability);
             return false;
